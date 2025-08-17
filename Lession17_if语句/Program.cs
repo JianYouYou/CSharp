c2e3c859ae9b6e1dd4a7dc16f5e072db468fdce9
Jianyouyou <1535298333@qq.com>
2025-08-17 16:17:51 +0800

    feat:添加了对if语句的学习

---
+++ Lession17_if语句/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Lession17_if语句
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("if语句");
+            //让顺序执行的代码 产生分支
+            //if(条件（bool类型))
+            //{
+            //条件为真时执行语句块
+            //}
+            //else
+            //{
+            //条件为假时执行else语句块
+            //}
+            //不需要写分号
+            //可以嵌套使用
+            #region 嵌套使用
+
+            #endregion
+        }
+    }
+}
