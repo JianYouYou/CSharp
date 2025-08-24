30c6fbc9b4605db3eb22d1d37136f3d516f8be5f
Jianyouyou <1535298333@qq.com>
2025-08-24 14:25:41 +0800

    feat：新增了对引用类型之一的string类型的特殊功能的补充

---
+++ LessionTwo5_值和引用类型/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace LessionTwo5_值和引用类型
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("值类型和引用类型");
+
+            #region 变量类型的复习
+            //无符号整型
+            byte b = 0;
+            ushort us = 0;
+            uint ui = 0;
+            ulong ul = 0;
+            //有符号整型
+            sbyte sb = 0;
+            short s = 0;
+            int i = 0;
+            long l = 0;
+            //浮点数
+            float f = 0.1f;
+            double d = 0.1;
+            decimal de = 0.1m;
+            //布尔
+            bool b1 = false;
+            bool b2 = true;
+            //字符
+            char c = 'a';
+            string str = "str";
+            //复杂数据类型
+            //enum 枚举
+            //一维数组 二维数组 交错数组
+
+            //上面学过的变量类型 分为 值类型 和 引用类型
+            //引用类型：string 数组 类（未学习）
+            //值类型：其他都是，结构体（未学习）
+
+            #endregion
+
+            #region 区别
+            //区别
+            //值类型存储在 栈空间 - 系统分配 自动回收 小而快
+            //引用类型存储在 堆控件 - 手动声明和释放 大而慢
+            int a = 10;
+            int[] arr = { 1, 2, 3, 4 };
+
+            int c1 = a;
+            int[] arr1 = arr;
+            Console.WriteLine("a={0},c1={1}", a, c1);
+            Console.WriteLine("arr[0]={0},arr1[0]={1}", arr[0], arr1[0]);
+            Console.WriteLine("修改后");
+            c1 = 20;
+            arr1[0] = 20;
+            Console.WriteLine("a={0},c1={1}", a, c1);
+            Console.WriteLine("arr[0]={0},arr1[0]={1}", arr[0], arr1[0]);
+
+            arr1 = new int[] { 99, 98, 97, 96 };
+            Console.WriteLine("arr[0]={0},arr1[0]={1}", arr[0], arr1[0]);
+            //使用 new int[] 是新建了一个堆内存 
+            #endregion
+        }
+    }
+}
---
+++ LessionTwo6_特殊的引用类型/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace LessionTwo6_特殊的引用类型
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("特殊的引用类型string");
+
+            #region 复习值类型 引用类型
+            //值类型 - 内存在栈里
+            //引用类型之外都是值类型 还有结构体
+            //引用类型 - 内存在堆里
+            //string 数组 类 枚举
+            #endregion
+
+            #region string的他变我不变
+            //与数组不同 新声明string 使其等于 以声明的string 这里在堆里会新建一个 内存 存储字符串 
+            //不会共用一个堆 自动new
+            string str = "123";
+            string str2 = str;
+            str = "456";
+            Console.WriteLine(str);
+            Console.WriteLine(str2);
+
+            //!!频繁的改变string 重新赋值 会导致之前的 在堆里的 内存滞留 不会 被系统回收 
+            //会导致内存垃圾 影响性能
+            #endregion
+
+            //通过断点 在监视窗口 可以查看内存信息
+        }
+    }
+}
