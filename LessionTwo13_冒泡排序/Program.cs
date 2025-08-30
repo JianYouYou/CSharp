a7a9dac9928e1350c98599c98d7c544446e622a5
Jianyouyou <1535298333@qq.com>
2025-08-30 21:05:48 +0800

    feat:新增对选择排序的学习。完成CSharp基础部分的学习

---
+++ LessionTwo13_冒泡排序/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace LessionTwo13_冒泡排序
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("冒泡排序");
+
+            #region 排序的基本概念
+            //将一组"无序"的记录序列 调整为"有序"的记录序列
+            //9 7 2 3 4 5 8 1
+            //变为(升序或降序)
+            //1 2 3 4 5 7 8 9
+            //9 8 7 5 4 3 2 1
+
+            //在程序中 序列一般存储在数组中
+            //所以 排序 往往是对数组进行排序
+            #endregion
+
+            #region 基本原理
+            //8 7 1 5 4 2 6 3 9 
+            //两两相邻 不停比较 不停交换 比较n轮
+            //每一都是i和i+1 然后 i++
+
+            #endregion
+
+            #region 代码实现
+            //int[] arr = { 1, 3, 6, 2, 6, 5, 3, 7, 9, 4 };
+            //for (int m = 1; m < arr.Length; m++)
+            //{
+            //    for (int i = 1; i < arr.Length - 1; i++)
+            //    {
+            //        int temp = arr[i];
+            //        if (arr[i] > arr[i + 1])
+            //        {
+            //            arr[i] = arr[i + 1];
+            //            arr[i + 1] = temp;
+            //        }
+            //    }
+            //}
+            //for (int i = 0; i < arr.Length; i++) 
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+            //优化 
+            //确定位置的数不必再比较
+            //确定一轮后 极值（最大或最小） 已经放到了对应的位置
+            //所以 每完成1轮 就可以少循环1轮
+            //每完成一大轮 每个小轮就少循环1遍
+
+            //int[] arr = { 1, 3, 6, 2, 6, 5, 3, 7, 9, 4 };
+            //for (int m = 1; m < arr.Length; m++)
+            //{
+            //    for (int i = 1; i < arr.Length - 1 - m; i++)//优化：再减一个m
+            //    {
+            //        int temp = arr[i];
+            //        if (arr[i] > arr[i + 1])
+            //        {
+            //            arr[i] = arr[i + 1];
+            //            arr[i + 1] = temp;
+            //        }
+            //    }
+            //}
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+
+            //特殊情况的优化
+            //如果数组为：9 1 2 3 4 5 6 7 8
+            //进行一次大循环就完成了 但是程序仍然会进行后续的循环
+            //会浪费性能
+            //可以添加一个条件判断是否进行了交换语句 如果没有进入就直接跳出循环
+
+            int[] arr = { 1, 3, 6, 2, 6, 5, 3, 7, 9, 4 };
+
+            for (int m = 1; m < arr.Length; m++)
+            {
+                bool what = false;
+                for (int i = 1; i < arr.Length - 1 - m; i++)//优化：再减一个m
+                {
+                    int temp = arr[i];
+                    if (arr[i] > arr[i + 1])
+                    {
+                        what = true;
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                    }
+                    
+                }
+                if (!what)
+                {
+                    break;
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            #endregion
+        }
+    }
+}
---
+++ LessionTwo14_选择排序/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace LessionTwo14_选择排序
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("选择排序");
+            #region 选择排序的基本原理
+            //建立中间商 依次比较 找出极值 放入目标位置 比较n轮
+            //建一个索引 指向第n个元素 
+            //用循环 大循环n次
+            //每个大循环建立一个索引
+            //从第i个(i = 0)开始 往后依次比 找到最大的 然后放到第n个位置
+
+            int[] arr = { 1, 4, 6, 9, 3, 5, 2, 6 };
+            //循环依次确定一个极值 所以第m次循环 极值放的位置是arr.Length - m;
+            for (int m = 0; m < arr.Length; m++)
+            {
+                //声明一个中间商 索引
+                int index = 0;
+                //找最大值
+                //已经固定的值不需要再比较一次 进行一大轮就确定一个 所以-m
+                for (int n = 1; n < arr.Length - m ; n++)
+                {
+                    if (arr[index] < arr[n])
+                    {
+                        index = n;
+                    }
+                }
+
+                //将极大值放在第arr.Length - m位置上
+                //判断得出的极值是否已经在该在的位置 不需要换 比如得到的最大值（9）就在最后一个
+                if (index != arr.Length - 1 - m )
+                {
+                    int temp = arr[index];
+                    arr[index] = arr[arr.Length - 1 - m];
+                    arr[arr.Length - 1 - m] = temp;
+
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+
+
+            #endregion
+        }
+    }
+}
