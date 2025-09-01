2b2493ebb04539bb486298bdd4702bd7d6ab8c52
Jianyouyou <1535298333@qq.com>
2025-09-01 16:55:42 +0800

    feat:新增了关于类的成员变量的声明的规则和学习了访问修饰符的使用方法

---
+++ LessionThere1_类和对象/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace LessionThere1_类和对象
+{
+    //万物皆对象
+    //用程序来抽象形容对象
+    //类是对象的模板 通过类声明对象
+    //类的关键词 class
+
+    //类在namespace命名空间里声明
+
+    //类的声明
+    //命名用帕斯卡命名法 首字母大写
+    //同一语句块里（namespace命名空间里）的不同类不能重名
+    class MyClass
+    {
+        //特征-成员变量
+        //行为-成员函数
+        //保护特征-成员属性
+
+        //构造函数 析构函数
+        //索引器
+        //运算符重载
+        //静态变量
+    }
+    class Person
+    {
+
+    }
+    class Mechine
+    {
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("类和对象");
+            //类创造类对象的行为 叫做实例化对象
+            //类对象 是引用类型
+
+            //类名 变量名;
+            //类名 变量名 = null;(null代表空)
+            //类名 变量名 = new 类名();        这是初始化了的 上面两个没有初始化
+
+            //实例化对象
+
+            Person p;
+            Person p1 = null;
+            Person p2 = new Person();
+            Person p3 = new Person();
+            //没初始化类变量 只在栈里占据了地址 并没有在堆里占用内存
+            //只有初始化之后 才在堆里占据内存空间 并有栈指向该堆
+            //虽然p2p3是同一个类的实例化 但是他们两个没有联系 都是独有的数据
+
+            //面向对象编程 是女娲模式。。 想要什么对象 就new什么对象
+        }
+    }
+}
---
+++ LessionThere2_成员变量和访问修饰符/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace LessionThere2_成员变量和访问修饰符
+{
+    //声明在类类语句块中
+    //用来描述对象的特征
+    //可以是任何类型
+    //数量没有限制
+    //是否赋值根据需求来定
+    class Person
+    {
+        //访问修饰符
+        //public--公共的 自己（内部）和别人（外部）都能访问和使用
+        //private--私有的 自己（）内部能使用 别人（外部）不能访问和使用
+        //protected--保护的 自己（内部）和子类才能访问和使用
+        //声明成员变量不设定访问修饰符 默认为private
+        //特征--成员变量
+        //血量
+        public int HP;
+        //姓名
+        public string Name = "clark";
+        //防御
+        public int DEF;
+        //攻击力
+        public int ATK;
+        //女朋友
+        //在类中声明一个和自己相同类型的成员变量时 
+        //不能对他进行实例化（初始化）
+        public Person GridFriend;
+    }
+
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("成员变量和访问修饰符");
+            //声明不初始化 会进行默认初始化
+            //值类型 整型这类的都是0 bool是false
+            //引用类型的是 null
+            //看默认初始化给的值的技巧(default(变量类型))
+            Console.WriteLine(default(int));
+            Console.WriteLine("*********************");
+            //使用成员变量
+            Person clark = new Person();
+            clark.HP = 100;
+            Console.WriteLine(clark.HP);
+        }
+    }
+}
