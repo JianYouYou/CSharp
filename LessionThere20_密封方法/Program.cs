5998b7d1176e09b2e169c42b81f4ea96c43d3a47
Jianyouyou <1535298333@qq.com>
2025-09-17 18:55:16 +0800

    feat:学习了sealed关键字对方法函数的使用方法，可以令虚函数和抽象函数无法再被继承

---
+++ LessionThere20_密封方法/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LessionThere20_密封方法
+{
+    //让抽象方法和虚方法 断子绝孙 不可被继承
+    //关键词 sealed
+    //配合抽象函数和虚函数使用
+    //虚函数关键字 virtual
+    //抽象关键字 abstract
+    //对override修饰的方法使用 sealed 可以让此方法不能再被继承
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("密封方法");
+        }
+    }
+}
