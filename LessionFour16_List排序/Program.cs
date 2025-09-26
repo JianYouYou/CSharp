edaa14d76c847b65e6de363f69829ff244910a95
Jianyouyou <1535298333@qq.com>
2025-09-26 18:41:06 +0800

    feat:学习了List排序，有List自带的排序功能，.Sort();当声明的List变量的类型是定义的类时，需要令类继承IComparable<>接口，并在类里实现CompareTo。这里有返回值的知识点。有更简单的方法是写一个比较功能的委托函数，自定义类的List变量使用.Sort();时在括号里调用这个委托函数，更简单的时直接在括号里用lambad，结合三目运算符，直接实现功能。

---
+++ LessionFour16_List排序/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+namespace LessionFour16_List排序
+{
+    #region 自己定义的类
+    //自己写的类 想要实现排序 需要继承Icomparable接口 并实现排序方法
+    class Test : IComparable<Test>  //带<>是泛型接口 有不带<>的非泛型 ，但是泛型更灵活
+    {
+        public int value;
+
+        public Test(int value)
+        {
+            this.value = value;
+        }
+
+        public int CompareTo(Test other)
+        {
+            //返回值
+            //小于0
+            //放在传入对象的前边
+            //等于0
+            //原位不变
+            //大于0
+            //放在传入对象后面
+
+            //简单理解为 一维坐标轴 传入对象在原点0 根据返回值不同 放在相应的位置
+            if (this.value > other.value)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+            //这里if else语句可以用三目运算符
+        }
+    }
+    #endregion
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("List排序");
+            #region List自带的排序
+            // .sort();
+            List<int> ints = new List<int>();
+            ints.Add(4);
+            ints.Add(2);
+            ints.Add(1);
+            ints.Add(3);
+            ints.Add(6);
+            ints.Add(5);
+            for (int i = 0; i < ints.Count; i++)
+            {
+                Console.WriteLine(ints[i]);
+            }
+            ints.Sort();
+            Console.WriteLine("华丽分割线");
+            for (int i = 0; i < ints.Count; i++)
+            {
+                Console.WriteLine(ints[i]);
+            }
+            #endregion
+
+            #region 自己定义的类排序
+            Console.WriteLine("华丽分割线");
+            List<Test> values = new List<Test>();
+            values.Add(new Test(20));
+            values.Add(new Test(70));
+            values.Add(new Test(10));
+            values.Add(new Test(90));
+            values.Sort();
+            for (int i = 0; i < values.Count; i++)
+            {
+                Console.WriteLine(values[i].value);
+            }
+            #endregion
+
+            #region 通过委托函数进行排序
+            Console.WriteLine("华丽分割线");
+
+            //写一个委托函数 在委托里实现比较 用Sort(委托函数);调用这个委托
+            //还可以用匿名函数 在Sort()括号里实现 
+            //更简单是用lambad 匿名的delagate都不用
+            #endregion
+
+        }
+    }
+}
